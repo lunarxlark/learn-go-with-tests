d639d236fb788f68e5219ae67d2071cd8e5f42d2
lunarxlark <lunarxlark@gmail.com>
2021-04-29 21:08:52 +0900

    Add Area(Circle)

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,5 +1,7 @@
 package structs_methods_interfaces
 
+import "math"
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -8,7 +10,15 @@ func Area(rectangle Rectangle) float64 {
 	return (rectangle.Width * rectangle.Height)
 }
 
+func Area(circle Circle) float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
