821c88aba1ca41474a86e1e42829676758d260fb
lunarxlark <lunarxlark@gmail.com>
2021-05-02 15:50:00 +0900

    add test for pointers&errors

---
+++ pointers_errors/wallet.go
@@ -0,0 +1,13 @@
+package pointers_errors
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(money int) {
+	w.balance += money
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
