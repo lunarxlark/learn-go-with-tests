41028bfb086d583cf10c8d560f50a83318c07bf6
lunarxlark <lunarxlark@gmail.com>
2021-05-02 15:23:51 +0900

    並び替え

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -2,6 +2,11 @@ package structs_methods_interfaces
 
 import "math"
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -14,17 +19,12 @@ func (r Rectangle) Area() float64 {
 	return (r.Width * r.Height)
 }
 
-func (c Circle) Area() float64 {
-	return math.Pi * c.Radius * c.Radius
-}
-
-type Rectangle struct {
-	Width  float64
-	Height float64
-}
-
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 type Triangle struct {
