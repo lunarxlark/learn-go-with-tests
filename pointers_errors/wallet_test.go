679c83b79e7a5aeaa148e30203579e96bed3c74a
lunarxlark <lunarxlark@gmail.com>
2021-05-03 11:10:19 +0900

    fix bug found by errcheck

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -12,8 +12,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(10)
+		err := wallet.Withdraw(10)
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -41,5 +42,12 @@ func assertError(t *testing.T, got, want error) {
 	}
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
