4e8aa1bc671633d2c34ddbfecbacb95347a1050e
lunarxlark <lunarxlark@gmail.com>
2021-05-03 11:57:26 +0900

    add test helper func for Dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,14 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
 	got := Search(dictionary, "test")
-	want := "this is just a test"
+	want := "this is just a testa"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
