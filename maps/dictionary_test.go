4ac7d442ecd4157094bc992dfc326f79159f3181
lunarxlark <lunarxlark@gmail.com>
2021-05-03 12:03:15 +0900

    change Search to method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search(dictionary, "test")
-	want := "this is just a testa"
+	got := dictionary.Search("test")
+	want := "this is just a test"
 
 	assertStrings(t, got, want)
 }
