45067e42627ad96ca5e5a240fa73350e24a8df02
lunarxlark <lunarxlark@gmail.com>
2021-05-03 12:18:25 +0900

    Add test for unknown word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,6 +1,10 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
+
+var ErrUnknownWord = errors.New("could not find the word you were looing for")
 
 func (d Dictionary) Search(word string) string {
 	return d[word]
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,15 +4,46 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, err := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		got, err := dictionary.Search("unknown")
+		want := "could not find the word you were looing for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+		assertStrings(t, err.Error(), want)
+		assertError(t, err, ErrUnknownWord)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("there is no error")
+	}
+	if got != want {
+		t.Error("got %q want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
