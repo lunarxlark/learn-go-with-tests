3de668404460f2f80715b62cc702555dbc6edf4c
lunarxlark <lunarxlark@gmail.com>
2021-05-05 14:16:47 +0900

    inject dependency into mocking.Countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -8,6 +8,18 @@ import (
 
 const finalWord = "Go!"
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(writer io.Writer) {
 	for i := 3; i > 0; i-- {
 		time.Sleep(1 * time.Second)
