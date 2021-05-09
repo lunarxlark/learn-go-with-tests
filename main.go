87822057a8e038e68c4468eb62a502be5e2adfb8
lunarxlark <lunarxlark@gmail.com>
2021-05-09 15:13:17 +0900

    fix

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"os"
-	"time"
-
-	"github.com/lunarxlark/learn-go-with-tests/mocking"
-)
-
 func main() {
-	//sleeper := mocking.DefaultSleeper{}
-	sleeper := *mocking.ConfigurableSleeper{1 * time.Second, time.Sleep}
-	mocking.Countdown(os.Stdout, sleeper)
 }
